7d0cdea5efe6ebcb680d724ff7eaf798cdb49ce8
strombreeding <speaker1403@gmail.com>
2024-05-30 18:25:48 +0900

    쉘스크립트 변경

--- 1day.sh
+++ 1day.sh
@@ -1,1 +1,5 @@
-npm run movie
+
+
+
+exec >> /home/ubuntu/dragon_IMAX_/cron.log 2>&1
+cd /home/ubuntu/dragon_IMAX_ && npm run movie
--- 5min.sh
+++ 5min.sh
@@ -1,2 +1,5 @@
-xvfb-run --server-args="-screen 0 1024x768x24" npm start
+
+
+exec >> /home/ubuntu/dragon_IMAX_/cron.log 2>&1
+cd /home/ubuntu/dragon_IMAX_ && xvfb-run --server-args="-screen 0 1024x768x24" npm start
 
