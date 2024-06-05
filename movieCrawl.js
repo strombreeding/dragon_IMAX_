a274ec84c8ecbc84923d68d46fcfaa9ca4f86e02
이진희 <speaker1403@gmail.com>
2024-06-05 09:26:53 +0900

    movieCrawl 셀렉터 못찾는 문제 재귀 함수 실행으로 해결

--- movieCrawl.js
+++ movieCrawl.js
@@ -27,7 +27,7 @@ const screenTypeSelector =
 const movieCrawl = async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    // executablePath: "/usr/bin/chromium-browser",
+    executablePath: "/usr/bin/chromium-browser",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
