aa19c7583028a25ab960c12c4dcefcda57e159b7
strombreeding <speaker1403@gmail.com>
2024-06-21 15:28:15 +0900

    인스턴스내 커밋

--- 5min.sh
+++ 5min.sh
@@ -1,5 +1,8 @@
 
 
 exec >> /home/ubuntu/dragon_IMAX_/cron.log 2>&1
+
+find /tmp/ -name 'puppeteer_dev_chrome_profile*' -type f -delete
+
 cd /home/ubuntu/dragon_IMAX_ && xvfb-run --server-args="-screen 0 1024x768x24" npm start
 
